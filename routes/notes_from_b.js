75474cc3e12c204f77855f44386caed1d0ac4f02
KentTakashima <168892936+KentTakashima@users.noreply.github.com>
2024-07-23 15:20:38 +0900

    Update notes_from_b.js

--- routes/notes_from_b.js
+++ routes/notes_from_b.js
@@ -5,7 +5,7 @@ const cors = require('cors'); // corsミドルウェアを追加
 
 // 接続情報を設定
 const { MongoClient } = require("mongodb");
-const uri = "mongodb+srv://matsunami2:password@test.zlsie9f.mongodb.net/?retryWrites=true&w=majority";
+const uri = "process.env.MONGODB_URI";
 const client = new MongoClient(uri);
 
 // corsミドルウェアを使用
